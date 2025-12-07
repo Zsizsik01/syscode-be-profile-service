2aac6c88172ff2c55665f7e797f60b32bfc46b3a
Zsolt Nyeste <zsolt.nyeste@spreadmonitor.com>
2025-12-07 23:49:34 +0100

    feat: update project

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,23 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Content-Length',
+      'X-Requested-With',
+      'Access-Control-Allow-Credentials',
+      'If-Match',
+      'If-None-Match',
+    ],
+    credentials: true,
+    preflightContinue: false,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
